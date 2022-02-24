e3cde3a768e62fb060f9143d8a5281b665119e94
shobhitkmr1 <shobhit.2408@gmail.com>
2022-02-24 22:04:25 +0530

    C# Codes

---
+++ Binary_search.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+int Search(int[] arr,int x)
+{
+    var i=0;
+    var j=arr.Length;
+    while(i!=j)
+    {
+        var m=(i+j)/2;
+        if(x==arr[m])
+        return m;
+        if(x<arr[m])
+        j=m;
+        else
+        i=m+1;
+    }
+    return -1;
+}
+var items = new[] {2,3,5,7,11,14,17};
+
+Console.WriteLine(Search(items,1));
+//print -1
+Console.WriteLine(Search(items,7));
+//print 3
+Console.WriteLine(Search(items,19));
+//print -1
+// *** Simplified speed test ***
+items = (new int[10000000]).Select((_,i) => i).ToArray();
+var count = 100;
+var start = DateTime.Now;
+for(int i=0; i<count; i++)
+Search(items,7777777);
+var delta = DateTime.Now - start;
+var milliseconds=delta.TotalMilliseconds / count;
+Console.WriteLine(milliseconds);
+// less than one millisecond timeout
